2a77cc841b4019d6be1b5567fef0c5aebf77cb2e
vchikalkin <djchikalkin@gmail.com>
2025-12-19 13:26:43 +0300

    feat: add Instagram video and image download support

--- apps/bot/src/utils/urls.ts
+++ apps/bot/src/utils/urls.ts
@@ -1,5 +1,9 @@
-import { TIKTOK_URL_REGEX } from '@/constants/regex';
+import { INSTAGRAM_URL_REGEX, TIKTOK_URL_REGEX } from '@/constants/regex';
 
 export function validateTikTokUrl(url: string) {
   return TIKTOK_URL_REGEX.test(url);
+}
+
+export function validateInstagramUrl(url: string) {
+  return INSTAGRAM_URL_REGEX.test(url);
 }
